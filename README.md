f27ef50e2b2e572b4bc14eabc751cc11798de647
MunKiJeon <jmk070994@gmail.com>
2024-03-07 22:41:52 +0900

    data type section

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
 2024.03.05 시작!
 # DratAndFlutter_Nomadcoders
 > Dart와 Flutter 강의 - Nomadcoders 버전
+
+> VS Code에서 FLutter(Dart 포함되 있음) SDK 다운 후 적용
+> [https://docs.flutter.dev/get-started/install/windows/desktop]
+* 사전 준비 내용
+* 소스 코드 관리를 위한 Windows 2.27 이상용 Git.
+* 텍스트 편집기 또는 IDE 구성
+* 인기 있는 옵션
+* VS Code용 Flutter 확장이 포함된 Visual Studio Code 1.77 이상 . <<난 이거 선택
+* IntelliJ용 Flutter 플러그인이 포함된 Android Studio 2023.1(Hedgehog) 이상 .
+* IntelliJ용 Flutter 플러그인이 포함된 IntelliJ IDEA 2023.1 이상 .
+
+1. Flutter SDK 설치
+- 방법은 "VS Code를 사용한 설치" 와 "다운로드 및 설치" (난 간편한 VS Code 사용설치)
+- Flutter를 설치하라는 VS Code 프롬프트
+    1. VS 코드를 시작합니다.
+    2. 명령 팔레트를 열려면 Control+ Shift+를 누르십시오 P.
+    3. 명령 팔레트 에 를 입력합니다 flutter.
+    4. Flutter: 새 프로젝트를 선택합니다 .
+    5. VS Code는 컴퓨터에서 Flutter SDK를 찾으라는 메시지를 표시합니다.
+     - Flutter SDK가 설치되어 있으면 SDK 찾기를 클릭하세요 .
+     - Flutter SDK가 설치되어 있지 않은 경우 SDK 다운로드를 클릭하세요 . (이 옵션은 개발 도구 필수 구성 요소 의 지시에 따라 Windows용 Git을 설치하지 않은 경우 Flutter 설치 페이지를 보냅니다 .)
+    6. 어떤 Flutter 템플릿을 선택하라는 메시지가 표시되면 ? , 무시해. 누르다 Esc. 개발 설정을 확인한 후 테스트 프로젝트를 생성할 수 있습니다.
+2. Flutter SDK 다운로드
+    1. Flutter SDK용 폴더 선택 대화 상자가 표시 되면 Flutter를 설치할 위치를 선택하세요. VS Code는 시작하려면 사용자 프로필에 배치합니다. 다른 위치를 선택하세요.
+    2. Flutter 복제를 클릭합니다 . Flutter를 다운로드하는 동안 VS Code는 다음 팝업 알림을 표시합니다.
+     - Downloading the Flutter SDK. This may take a few minutes.
+    3. Flutter 다운로드가 완료되면 출력 패널이 표시됩니다.
+     - Checking Dart SDK version...
+     - Downloading Dart SDK from the Flutter engine ...
+     - Expanding downloaded archive...
+     + 성공하면 VS Code는 다음 팝업 알림을 표시합니다.
+     - Initializing the Flutter SDK. This may take a few minutes.
+     + 초기화하는 동안 출력 패널에 다음이 표시됩니다.
+     - Building flutter tool...
+     - Running pub upgrade...
+     - Resolving dependencies...
+     - Got dependencies.
+     - Downloading Material fonts...
+     - Downloading Gradle Wrapper...
+     - Downloading package sky_engine...
+     - Downloading flutter_patched_sdk tools...
+     - Downloading flutter_patched_sdk_product tools...
+     - Downloading windows-x64 tools...
+     - Downloading windows-x64/font-subset tools...
+     + Flutter 설치가 성공하면 VS Code는 다음 팝업 알림을 표시합니다.
+     - Do you want to add the Flutter SDK to PATH so it's accessible
+     - in external terminals?
+    4. PATH에 SDK 추가를 클릭합니다 .
+     + 성공하면 다음과 같은 알림이 표시됩니다.
+    5. VS Code는 Google Analytics 알림을 표시할 수 있습니다. 동의하면 확인 을 클릭합니다 .
+    6. VS Code 다시 시작!
--- main.dart
+++ main.dart
@@ -1,3 +1,106 @@
 void main(){
-  print('hello world');
+  /* -----------#1 VARIABLES------------- */
+  // print('hello world~~~');
+  
+  // var name =  '문키'; // var : 함수나 메소드 안에 지역변수로 많이 사용
+  String name = '문키';
+  // name = 1; 혹은 name = true; 안됨 
+  //=> 선언을 스트링 타입으로 먼저 선언 했기 때문에 타입을 맞춰야 함
+  name = 'munki';
+
+  //dynamic =  어떤타입이 들어올지 모를때 사용
+  //-> 여러타입을 다 가능케함 *정말 필요할때에 만 사용할것!!
+  //dynamic dy_name; 
+  //타입이 명확해 지면 해당 타입의 속성을 쓸수있음 
+  // if(dy_name is String){ dy_name.length} 
+  // if(dy_name is int){ dy_name.isEven}
+
+  //nullsafety
+  String? mk = 'munki';//? 를 붙이면 mk가 String 일수도 있고 null 일수도 있다를 표현 함
+  mk = null;
+  // mk.length; //mk가 null일수 있어서 사용 불가하다고 알려줌
+  if(mk != null){ mk.isNotEmpty; }//mk가 null 이 아님을 명시 했기 때문에 null일 경우를 무시함 
+  mk?.isNotEmpty;//이렇게도 가능함
+
+  final fin_Var = 'like const';//한번 선언 및 값이 정의된 이후 변경 불가함
+  final int fin_Int = 1234;//명확하게 자료형을 지정해 줄수도 있음
+
+  final googoo;
+
+  late final String gogogaga;
+  //do something, go to api = API에어 한번 값을 받아 변경 불가하게 사용 할때 사용
+  // print(gogogaga); // 아직 데이터를 정의하지 않았기에 사용 불가
+  gogogaga = 'lateData';
+
+  const con_val = '고정';//final과 동일해 보이지만 컴파일 전에 알고있는 값(API 값 같은?) 을 사용 해야한다.
+
+  print('hello '+ name);
+  print('hello '+ fin_Var);
+  print(fin_Int);
+
+/* ------------#2 DATA TYPES------------ */
+  String str = "aaa";
+  bool alive = true;
+  int age = 12;
+  double money = 60.99;
+
+  num x = 12; // int double 의 상위 클래스 자료형
+  x = 1000.199; // 정수 인지 실수인지 정확하게 구분
+
+  var giveMeFive = true;
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    if(giveMeFive) 5, //collection if
+    ];
+  // List<int> numbers = [1,2,3,4,];
+  numbers.first;// 1
+  numbers.last;//  5
+
+  var myName = 'MunKi';
+  var myAge = 25;
+  var greeting = "hello My name is $myName, and I\'m ${myAge + 6 }";
+  //단순히 변수 사용은 앞에 "$" 표시, 연산을 적용하려면 "${}" 사용
+  print(greeting);
+  
+  var oldFriends = ['soo','saa'];
+  var newFriends = [
+    'gogi',
+    'welsi',
+    'koki',
+    for (var friend in oldFriends) "👍 $friend",
+  ];
+  print(newFriends);
+
+  // var player = { //단순한 맵 구조는 key 와 Value 로 되어있음
+  //   'name': 'Tomi',
+  //   'exp': 98.95,
+  //   'lv': 23,
+  // };
+  // Map<List<int>,bool> player = { //데이터 타입을 지정해 줄수있음
+  //   [1,2,3,5,6]:true,
+  //   [4,5,6,7,6]:false,
+  //   [3,4,5,6,8]:true,
+  // };
+  List<Map<String,Object>> player = [ //리스트 안에 타입으로 지정해 줄수도 있음
+    {'name':'komodo'},
+    {'coordinate':[0,0,]},
+    {'hp':'komodo'},
+  ]; //이런식의 복잡한 데이터는 class로 처리하는게 더 좋음
+
+  Set<int> uniqueNumbers = {1,2,3,4,5,};
+  uniqueNumbers.add(1);
+  uniqueNumbers.add(1);
+  uniqueNumbers.add(1);
+  uniqueNumbers.add(1);
+  List<int> unUniqueNumbers = [1,2,3,4,5,];
+  unUniqueNumbers.add(1);
+  unUniqueNumbers.add(1);
+  unUniqueNumbers.add(1);
+  unUniqueNumbers.add(1);
+  print("SET value: $uniqueNumbers");
+  print("List valur: $unUniqueNumbers");
+  //Set과 List의 차이는 가지고 있는 아이템의 Unique(아이탬이 하나만 존재함) 차이이다
 }
